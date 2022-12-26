c6c4a1e6206a3b803a7bbd07e031b9cd6cf68675
Your Name <you@example.com>
2022-12-26 18:09:22 +0100

    44

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -5,12 +5,12 @@ const farms: FarmConfig[] = [
   {
     pid: 0,
     risk: 5,
-    lpSymbol: 'WAIFU-fUSDT',
+    lpSymbol: 'BANHMI-fUSDT',
     lpAddresses: {
       250: '0xC4045e90f65dD0405aC6CeAb18Eb37C29A1Bb990',
       97: '',
     },
-    tokenSymbol: 'WAIFU',
+    tokenSymbol: 'BANHMI',
     tokenAddresses: {
       250: '0xa99a2A440494299476f4ceB1696433c66c0d7358',
       97: '',
@@ -21,12 +21,12 @@ const farms: FarmConfig[] = [
   {
     pid: 1,
     risk: 5,
-    lpSymbol: 'WAIFU-WFTM',
+    lpSymbol: 'BANHMI-WFTM',
     lpAddresses: {
       250: '0x1A07372a4B17040BDeeCD416249C1171974199da',
       97: '',
     },
-    tokenSymbol: 'WAIFU',
+    tokenSymbol: 'BANHMI',
     tokenAddresses: {
       250: '0xa99a2A440494299476f4ceB1696433c66c0d7358',
       97: '',
@@ -102,12 +102,12 @@ const farms: FarmConfig[] = [
     pid: 6,
     risk: 5,
     isTokenOnly: true,
-    lpSymbol: 'WAIFU',
+    lpSymbol: 'BANHMI',
     lpAddresses: {
       250: '0xC4045e90f65dD0405aC6CeAb18Eb37C29A1Bb990',
-      97: '', // WAIFU-USDT LP
+      97: '', // BANHMI-USDT LP
     },
-    tokenSymbol: 'WAIFU',
+    tokenSymbol: 'BANHMI',
     tokenAddresses: {
       250: '0xa99a2A440494299476f4ceB1696433c66c0d7358',
       97: '',
