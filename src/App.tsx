aa572a745378e7c5df9e98ba11cc2f292275e528
Your mom <you@example.com>
2023-12-01 20:19:56 +0100

    11

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const App: React.FC = () => {
             <Route path="/" exact>
               <Home />
             </Route>
-            <Route path="/farms">
+            <Route path="/farmss">
               <Farms />
             </Route>
             <Route path="/nests">
